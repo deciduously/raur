db1c15ca6b9d63d7502fa7b40d45bea2e1fff807
deciduously <bendlovy@gmail.com>
2017-01-06 10:17:03 -0500

    add travis.yml

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,17 @@
 extern crate hyper;
 
 use hyper::client;
+use std::io::Read;
 
-pub fn search_by_name(name: &str) -> hyper::status::StatusCode {
+pub fn search_by_name(name: &str) -> String {
     let url = "/rpc/?v=5&type=search&by=";
     let field = "name";
     let query = format!("https://aur.archlinux.org{}{}&arg={}", url, field, name);
     let client = client::Client::new();
-    let res = client.get(&query).send().unwrap();
-    return res.status;
+    let mut res = client.get(&query).send().unwrap();
+    let mut buf = String::new();
+    res.read_to_string(&mut buf);
+    buf
 }
 
 #[cfg(test)]
