27e8b63a8eb4a440ca0b84b33bc61d71e2ff2b5a
deciduously <bendlovy@gmail.com>
2017-01-06 11:28:14 -0500

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 #![cfg_attr(feature="clippy", feature(plugin))]
 #![cfg_attr(feature="clippy", plugin(clippy))]
 
+#[macro_use]
+extern crate clap;
 extern crate raur;
 
-use std::env;
-
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let keywords = &args[1]; //package to search for
-
-    println!("{}", raur::search_by_name(keywords));
+    let matches = clap_app!(raur =>
+        (version: "0.1.0")
+        (author: "deciduously <bendlovy@gmail.com>")
+        (about: "Simple CLI AUR helper")
+        (@arg DOWNLOAD: -d --download +takes_value "Downloads the selected package to your build folder")
+        (@arg SEARCH: -s --search +takes_value "Searches AUR for given package and returns info")
+    ).get_matches();
+        
+    println!("doing thing on {}", matches.value_of("DOWNLOAD").unwrap());
 }
